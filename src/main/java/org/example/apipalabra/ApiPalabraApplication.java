02420e573bff32ca74dfe1000391911f8be6c76e
out0f4mm0 <eriking2004@gmail.com>
2023-12-14 10:14:05 +0100

    Init repo and DB made

---
+++ src/main/java/org/example/apipalabra/ApiPalabraApplication.java
@@ -0,0 +1,13 @@
+package org.example.apipalabra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPalabraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPalabraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/apipalabra/ApiPalabraApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.apipalabra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPalabraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
